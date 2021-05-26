ad3d719887bf2fad7e02a2b1723f92f73275750e
天² <84515646+mrmatianfang@users.noreply.github.com>
2021-05-26 21:19:46 +0800

    Update 11-散列1 电话聊天狂人.cpp

--- DataStructure/PTA_DataStructure_2021_Spring/11-散列1 电话聊天狂人.cpp
+++ DataStructure/PTA_DataStructure_2021_Spring/11-散列1 电话聊天狂人.cpp
@@ -1,3 +1,110 @@
+// my hash map
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct phoneNumber{
+    string number;
+    int times;
+    phoneNumber(string num):number(num),times(1){}
+};
+
+class HashMap{
+private:
+    const int base = 100000;
+public:
+    vector<vector<phoneNumber>>numbers;
+
+    HashMap()
+    {
+        numbers.resize(base);
+    }
+
+    int getHash(string number);
+    int insert(string number);
+};
+
+int HashMap::getHash(string number)
+{
+    return stoi(number.substr(6))%base;
+}
+
+int HashMap::insert(string number)
+{
+    int hash = getHash(number);
+    for(auto &num:numbers[hash])
+    {
+        if(num.number==number)
+        {
+            num.times+=1;
+            return num.times;
+        }
+    }
+    numbers[hash].push_back(phoneNumber(number));
+    return 1;
+}
+
+int main()
+{
+    int n = 0;
+    scanf("%d",&n);
+
+    HashMap h = HashMap();
+    int nums = 0;
+    int maxTime = 0;
+    string result="123456789101";
+
+    for(int i = 0;i<n;++i)
+    {
+        string from,to;
+        cin>>from>>to;
+
+        int t1 = h.insert(from);
+        if(t1>maxTime)
+        {
+            nums = 1;
+            maxTime = t1;
+            result = from;
+        }
+        else if(t1==maxTime)
+        {
+            nums+=1;
+            if(result>from)
+            {
+                result = from;
+            }
+        }
+        int t2 = h.insert(to);
+        if(t2>maxTime)
+        {
+            nums = 1;
+            maxTime = t2;
+            result = to;
+        }
+        else if(t2==maxTime)
+        {
+            nums+=1;
+            if(result>to)
+            {
+                result = to;
+            }
+        }
+    }
+
+    cout<<result<<" "<<maxTime;
+    if(nums!=1)
+    {
+        cout<<" "<<nums;
+    }
+
+    return 0;
+}
+
+
+// unordered_map
 #include <iostream>
 #include <string>
 #include <vector>
